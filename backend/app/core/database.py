0c10ad4ac23318a78dc7c0d78018201f9b3382c0
Mikhail Shestakov <mishail258@mail.ru>
2026-06-25 15:46:28 +0500

    исправил опечатку

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
-from app.core.conifg import settings
+from app.core.config import settings
 
 engine = create_async_engine(
     settings.DATABASE_URL,
